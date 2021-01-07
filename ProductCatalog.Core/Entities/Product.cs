c3e87b479ecada8fb2803ef7a136a4cfdaf65745
Abdelrahman Kinani <a.kinani@arco.sa>
2021-01-07 16:33:46 +0200

    Add domain models and dbContext

---
+++ ProductCatalog.Core/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductCatalog.Core.Entities
+{
+    [Table("Product")]
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Price { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ ProductCatalog.Infrastructure/Data/ProductCatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ProductCatalog.Infrastructure.Data
+{
+    public class ProductCatalogContext : DbContext
+    {
+        public ProductCatalogContext(DbContextOptions<ProductCatalogContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
