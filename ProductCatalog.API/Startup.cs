7352cef6162f743157b64c984e2de510a743b4ed
Abdelrahman Kinani <a.kinani@arco.sa>
2021-01-07 16:56:58 +0200

    update startup configure services order

--- ProductCatalog.API/Startup.cs
+++ ProductCatalog.API/Startup.cs
@@ -27,12 +27,12 @@ namespace ProductCatalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            
             services.AddDbContext<ProductCatalogContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("ProductCatalogConnection"));
             });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
